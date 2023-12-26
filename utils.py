f83fc8f98e935595b75750e097e169c794228019
Jan du Plessis <drjanduplessis@icloud.com>
2023-12-26 10:34:53 +0000

    Automated commit via Python script - 2023-12-26 10:34

--- utils.py
+++ utils.py
@@ -42,7 +42,7 @@ init(autoreset=True)
 def time_it(func):
     def wrapper(*args, **kwargs):
         start_time = time.time()
-        print(f"{Fore.YELLOW}[F] FUCTION: {func.__name__}()")
+        print(f"{Fore.YELLOW}[🏁] FUCTION: {func.__name__}()")
         result = func(*args, **kwargs)
         print(
             f"{Fore.GREEN}{Style.DIM}[✔️] Completed: {func.__name__}() - Time taken: {time.time() - start_time:.2f} seconds"
